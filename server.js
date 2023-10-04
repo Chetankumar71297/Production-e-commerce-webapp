32ee062302c1ea59918a7970f57a70c0695c620f
Chetan kumar <chetankumar71297@gmail.com>
2023-10-04 19:11:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ import { fileURLToPath } from "url";
 
 //esmodule fix(deployment bug)
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+//const __dirname = path.dirname(__filename);
+const __dirname = path.resolve()
 //configure env
 dotenv.config();
 
@@ -26,16 +26,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
+//app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
-
+app.use(express.static(path.join(__dirname, "./client/build")));
 //rest api
 app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/static/index.html"));
 });
 
 //PORT
