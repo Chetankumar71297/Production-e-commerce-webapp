f6da652228773d94dc37097da1924ed511e5b1a5
Chetan kumar <chetankumar71297@gmail.com>
2023-10-04 18:40:19 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(mongodb+srv://chetankumar71297:*****@cluster0.sl2iysr.mongodb.net/);
+    const conn = await mongoose.connect("mongodb+srv://chetankumar71297:chetan1234@cluster0.sl2iysr.mongodb.net/?retryWrites=true&w=majority");
     console.log(`Connected To Mongodb Database ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error in Mongodb ${error}`);
